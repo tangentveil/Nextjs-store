7e5f5dd004a811d796beacf1f415c7326cc801b3
tangentveil <anirudha0789@gmail.com>
2025-02-06 14:49:08 +0530

    .

--- components/form/Buttons.tsx
+++ components/form/Buttons.tsx
@@ -6,7 +6,7 @@ import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { SignInButton } from "@clerk/nextjs";
 import { FaRegHeart, FaHeart } from "react-icons/fa";
-import { LuTrash2, LuPenSquare } from "react-icons/lu";
+import { LuTrash2 } from "react-icons/lu";
 
 type btnSize = "default" | "lg" | "sm";
 
