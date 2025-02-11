1608b670134f9c47f5c7f45217b97b74a262c807
tangentveil <anirudha0789@gmail.com>
2025-02-11 15:22:57 +0530

    sales page

--- app/admin/products/[id]/edit/page.tsx
+++ app/admin/products/[id]/edit/page.tsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import {
+  fetchAdminProductDetails,
+  updateProductAction,
+  updateProductImageAction,
+} from "@/utils/actions";
+import FormContainer from "@/components/form/FormContainer";
+import FormInput from "@/components/form/FormInput";
+import PriceInput from "@/components/form/PriceInput";
+import TextAreaInput from "@/components/form/TextAreaInput";
+import { SubmitButton } from "@/components/form/Buttons";
+import CheckboxInput from "@/components/form/CheckboxInput";
+import ImageInputContainer from "@/components/form/ImageInputContainer";
 
-function EditProductPage() {
+async function EditProductPage({ params }: { params: { id: string } }) {
+  const { id } = params;
+  const product = await fetchAdminProductDetails(id);
+  const { name, company, description, featured, price } = product;
+
   return (
-    <div>EditProductPage</div>
-  )
+    <section>
+      <h1 className="text-2xl font-semibold mb-8 capitalize">Update Product</h1>
+      <div className="border p-8 rounded">
+        <ImageInputContainer
+          action={updateProductImageAction}
+          name={name}
+          image={product.image}
+          text="update image"
+        >
+          <input type="hidden" name="id" value={id} />
+          <input type="hidden" name="url" value={product.image} />
+        </ImageInputContainer>
+        <FormContainer action={updateProductAction}>
+          <div className="grid gap-4 md:grid-cols-2 my-2">
+            <input type="hidden" name="id" value={id} />
+            <FormInput
+              type="text"
+              name="name"
+              label="product name"
+              defaultValue={name}
+            />
+            <FormInput
+              type="text"
+              name="company"
+              // label="product name"
+              defaultValue={company}
+            />
+            <PriceInput defaultValue={price} />
+            <TextAreaInput
+              name="description"
+              labelText="product description"
+              defaultValue={description}
+            />
+          </div>
+
+          <div className="mt-6">
+            <CheckboxInput
+              name="featured"
+              defaultChecked={featured}
+              label="featured"
+            />
+          </div>
+          <SubmitButton text="update product" className="mt-8" />
+        </FormContainer>
+      </div>
+    </section>
+  );
 }
 
-export default EditProductPage
+export default EditProductPage;
--- utils/actions.ts
+++ utils/actions.ts
@@ -4,7 +4,7 @@ import db from "@/utils/db";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { imageSchema, productSchema, validateWithZodSchema } from "./schemas";
-import { uploadImage } from "./supabase";
+import { deleteImage, uploadImage } from "./supabase";
 import { revalidatePath } from "next/cache";
 
 const getAuthUser = async () => {
@@ -108,14 +108,83 @@ export const deleteProductAction = async (prevState: { productId: string }) => {
   await getAdminUser();
 
   try {
-    await db.product.delete({
+    const product = await db.product.delete({
       where: {
         id: productId,
       },
     });
 
+    await deleteImage(product.image);
     revalidatePath("/admin/products");
     return { message: "product removed" };
+  } catch (error) {
+    return renderError(error);
+  }
+};
+
+export const fetchAdminProductDetails = async (productId: string) => {
+  await getAdminUser();
+
+  const product = await db.product.findUnique({
+    where: {
+      id: productId,
+    },
+  });
+
+  if (!product) redirect("/admin/products");
+  return product;
+};
+
+export const updateProductAction = async (
+  prevState: any,
+  formData: FormData
+) => {
+  await getAdminUser();
+
+  try {
+    const productId = formData.get("id") as string;
+    const rawData = Object.fromEntries(formData);
+    const validatedFields = validateWithZodSchema(productSchema, rawData);
+
+    await db.product.update({
+      where: {
+        id: productId,
+      },
+      data: {
+        ...validatedFields,
+      },
+    });
+
+    revalidatePath(`/admin/products/${productId}/edit`);
+    return { message: "Product Image Updated successfully" };
+  } catch (error) {
+    return renderError(error);
+  }
+};
+
+export const updateProductImageAction = async (
+  prevState: any,
+  formData: FormData
+) => {
+  await getAuthUser();
+  try {
+    const image = formData.get("image") as File;
+    const productId = formData.get("id") as string;
+    const oldImageUrl = formData.get("url") as string;
+
+    const validatedFile = validateWithZodSchema(imageSchema, { image });
+    const fullPath = await uploadImage(validatedFile.image);
+    await deleteImage(oldImageUrl);
+    await db.product.update({
+      where: {
+        id: productId,
+      },
+      data: {
+        image: fullPath,
+      },
+    });
+    revalidatePath(`/admin/products/${productId}/edit`);
+    return { message: "Product Image updated successfully" };
   } catch (error) {
     return renderError(error);
   }
